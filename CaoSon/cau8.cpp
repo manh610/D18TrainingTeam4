03d87c960ad2bd83b134376a5c05a3120eec53b0
sonnhthtb <43534240+sonnhthtb@users.noreply.github.com>
2018-09-26 21:59:47 +0700

    Update cau8.cpp

--- CaoSon/cau8.cpp
+++ CaoSon/cau8.cpp
@@ -6,7 +6,7 @@ int main(void)
 	float tong;
 	printf("nhap vao 2 gia tri a,b lan luot:");
 	scanf("%d%d",&a,&b);
-	tong=(((a/7)+(a*a*b)-(sqrt(abs(a-b)))+b*b*b*b*b)*(2*a+4*b));
+	tong=(((a/7)+(a*a*b)-(sqrt(abs(float)a-b)))+b*b*b*b*b)*(2*a+4*b));
 	printf("tong la:%.4f",tong);
 	return 0;
 }
