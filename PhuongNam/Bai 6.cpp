426f52e721bead18d3041d392a868e8039a8d23f
namvu123 <43583158+namvu123@users.noreply.github.com>
2018-09-26 20:56:02 +0700

    tuan1

---
+++ PhuongNam/Bai 6.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int namsinh,tuoi;
+	printf("nam sinh cua ban la:");
+	scanf("%d",&namsinh);
+	tuoi=2018-namsinh;
+	printf("tuoi cua ban la:%d",tuoi);
+	return 0;
+}
