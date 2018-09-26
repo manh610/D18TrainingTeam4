f20fa01798fdf908b026c391df3c0b8986cbb9a9
trungphuong2k <43600102+trungphuong2k@users.noreply.github.com>
2018-09-26 20:39:56 +0700

    tuần 1

---
+++ TrungPhuong/bài 3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#define PI 3.14159
+int main (){
+	int r;
+	float chuvi ,dientich;
+	printf("nhap ban kinh r:");
+	scanf("%d", &r);
+	chuvi= 2*PI*r;
+	dientich= PI*r*r;
+	printf("chu vi hinh tron la:%f\n",chuvi);
+	printf("dien tich hinh tron la:%f", dientich);
+	getch();
+	}
