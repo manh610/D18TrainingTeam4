508033f629929ef37dd2b1a9d37b5e8ebbc176cb
datnguyend18 <43572616+datnguyend18@users.noreply.github.com>
2018-09-26 22:06:43 +0700

    Tuần 1

---
+++ ThanhDat/Bài 5.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main (void) {
+	int a, b, c;
+	long long tong3canh;
+	printf("CHUONG TRINH TINH TONG CAC CANH HINH HOP CHU NHAT \n");
+	printf("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \n");
+	printf("Nhap ba so a, b, c: ");
+	scanf("%d%d%d",&a, &b, &c);
+	tong3canh= (sqrt(a*b*c)/a + sqrt(a*b*c)/ b + sqrt(a*b*c) / c) * 4;
+	printf("Tong ba canh cua hinh hop chu nhat la: %lld\n", tong3canh);
+	return 0;
+}
+
