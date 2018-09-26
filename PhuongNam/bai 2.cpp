99a81bc211e9ec8a625420183e665aa5c0fcb8c5
namvu123 <43583158+namvu123@users.noreply.github.com>
2018-09-26 19:36:52 +0700

    tuan1

---
+++ PhuongNam/bai 2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int  main()
+{
+	int a;
+	 long long b;
+	char c;
+	float d;
+	double e;
+	scanf("%d %lld %c %f %lf",&a,&b,&x,&d,&e);
+	printf("\n%d \n%lld \n%c \n%.3f \n%.9lf",a,b,c,d,e);
+	return 0;
+	}
